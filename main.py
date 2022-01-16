7613e84616893595901e37834b6c58c64c8a7aaa
Chinmay Tiwari <chinmaytanmay@outlook.com>
2022-01-16 22:05:26 +0000

    Added some small changes

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import pygame
 import wireframe
+import numpy as np
 
 class ProView:
   """Display 3d obj's in pygame!"""
-  def __init__(self,width,height):
+  def __init__(self,width,height,title):
     self.width = width
     self.height = height
     self.screen = pygame.display.set_mode((width,height))
-    pygame.display.set_caption("Wireframe")
+    pygame.display.set_caption(title)
     self.background = (10,10,50)
-
+    self.title = title
     self.wireframes = {}
     self.displayNodes = True
     self.displayEdges = True
@@ -22,8 +23,16 @@ class ProView:
       pygame.K_RIGHT:  (lambda x: x.translateAll('x',  10)),
       pygame.K_DOWN:   (lambda x: x.translateAll('y',  10)),
       pygame.K_UP:     (lambda x: x.translateAll('y', -10)),
+      pygame.K_SPACE:  (lambda x: x.translateAll('z', 10)),
+      pygame.K_b: (lambda x: x.translateAll('z', -10)),
       pygame.K_EQUALS: (lambda x: x.scaleAll(1.25)),
-      pygame.K_MINUS:  (lambda x: x.scaleAll( 0.8))
+      pygame.K_MINUS:  (lambda x: x.scaleAll( 0.8)),
+      pygame.K_q: (lambda x: x.rotateAll('X',  0.1)),
+      pygame.K_w: (lambda x: x.rotateAll('X', -0.1)),
+      pygame.K_a: (lambda x: x.rotateAll('Y',  0.1)),
+      pygame.K_s: (lambda x: x.rotateAll('Y', -0.1)),
+      pygame.K_z: (lambda x: x.rotateAll('Z',  0.1)),
+      pygame.K_x: (lambda x: x.rotateAll('Z', -0.1))
     }
     """Create window"""
     running = True
@@ -34,6 +43,7 @@ class ProView:
         elif event.type == pygame.KEYDOWN:
           if event.key in key_to_function:
             key_to_function[event.key](self)
+            print(event.key)
       self.screen.fill(self.background)
       self.display()
       pygame.display.flip()
@@ -63,16 +73,27 @@ class ProView:
     centerY = self.height/2
     for wireframe in self.wireframes.itervalues():
       wireframe.scale((centerX,centerY),scale)
-      
+  def rotateAll(self,axis,theta):
+    """same as last except rotation"""
+    rotateFunction = 'rotate' + axis
+    for wireframe in self.wireframes.itervalues():
+      center = wireframe.findCenter()
+      getattr(wireframe,rotateFunction)(center,theta)
       
       
 if __name__ == '__main__':
   cube = wireframe.Wireframe()
+  cube_nodes = []
+  #file = "teapot.obj"
+  #cube.read_obj()
+  
+  cube.addNodes(cube_nodes)
   cube.addNodes([(x ,y, z) for x in (50, 250) for y in (50, 250) for z in (50, 250)])
 
   cube.addEdges([(n,n+4) for n in range(0,4)]+[(n,n+1) for n in range(0,8,2)]+[(n,n+2) for n in (0,1,4,5)])
+
   
-  pv = ProView(400,300)
+  pv = ProView(400,300,"wireframe test")
   pv.addWireFrame('cube',cube)
   pv.run()
   
--- wireframe.py
+++ wireframe.py
@@ -1,5 +1,10 @@
+import math
+import os as o
 
 
+import numpy
+
+import matplotlib
 class Node:
   def __init__(self,coordinates):
     self.x = coordinates[0]
@@ -35,6 +40,79 @@ class Wireframe:
       node.x = centerX+scale*(node.x-centerX)
       node.y = centerY+scale*(node.y-centerY)
       node.z *=scale
+  def findCenter(self):
+    """find center"""
+    num_nodes = len(self.nodes)
+    meanX = sum([node.x for node in self.nodes])/num_nodes
+    meanY = sum([node.y for node in self.nodes])/num_nodes
+    meanZ = sum([node.z for node in self.nodes])/num_nodes
+    return (meanX,meanY,meanZ)
+  def rotateX(self, (cx,cy,cz), radians):
+    for node in self.nodes:
+        y      = node.y - cy
+        z      = node.z - cz
+        d      = math.hypot(y, z)
+        theta  = math.atan2(y, z) + radians
+        node.z = cz + d * math.cos(theta)
+        node.y = cy + d * math.sin(theta)
+  def rotateY(self, (cx,cy,cz), radians):
+    for node in self.nodes:
+        x      = node.x - cx
+        z      = node.z - cz
+        d      = math.hypot(x, z)
+        theta  = math.atan2(x, z) + radians
+        node.z = cz + d * math.cos(theta)
+        node.x = cx + d * math.sin(theta)
+  def rotateZ(self,(cx,cy,cz),radians):
+    for node in self.nodes:
+      x = node.x-cx
+      y = node.y-cy
+      z = node.z-cz
+      d = math.hypot(y,x)
+      theta = math.atan2(y,x) + radians
+      node.x = cx+d*math.cos(theta)
+      node.y = cy+d*math.sin(theta)
+  # def readObj(self, file):
+  #   try:
+      
+  #     File_object = open(r"teapot.obj", "r")
+  #     for line in File_object:
+  #       File_object.readline()
+        
+  #     cube_nodes = [x,y,z]
+  #   except OSError:
+  #     print("File not found.")
+  #   finally:
+  #     print("Read finished with or without errors.")
+  # def read_obj(in_file):
+  #   vertices = []
+  #   faces = []
+  #   in_file = open("teapot.obj")
+  #   try:
+  #     line = 0
+  #     for k, v in in_file.readline(line+1):
+  #         if k == 'v':
+  #             cube_nodes.append(v)
+  #         elif k == 'f':
+  #             for i in v:
+                
+  #               cube.addEdges(i)
+  #         else:
+            
+  #           print("epic loser fail.")
+  #         line += 1
+        
+  #   except:
+  #     print("fail.")
+    # if not len(faces) or not len(vertices):
+    #     return None
+
+    # pos = torch.tensor(vertices, dtype=torch.float)
+    # face = torch.tensor(faces, dtype=torch.long).t().contiguous()
+
+    # data = Data(pos=pos, face=face)
+
+    
   #debug func
   def outputnode(self):
     print"\n ----Nodes-----"
@@ -52,13 +130,14 @@ class Wireframe:
 
 #cube
 if __name__ == "__main__":
-  cube_nodes = [(x,y,z) for x in (0,1) for y in (0,1) for z in (0,1)]
+  cube_nodes = []
+  Wireframe.read_Obj("teapot.obj")
   cube = Wireframe()
   cube.addNodes(cube_nodes)
-  cube.addEdges([(n,n+4) for n in range(0,4)])
-  cube.addEdges([(n,n+1) for n in range(0,8,2)])
-  cube.addEdges([(n,n+2) for n in (0,1,4,5)])
+  # cube.addEdges([(n,n+4) for n in range(0,4)])
+  # cube.addEdges([(n,n+1) for n in range(0,8,2)])
+  # cube.addEdges([(n,n+2) for n in (0,1,4,5)])
     
-  cube.outputNodes()
-  cube.outputEdges()
+  # cube.outputNodes()
+  # cube.outputEdges()
 
